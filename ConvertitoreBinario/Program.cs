7496806d0ec2507e015fdc6b5225ee672f304b6b
Daniele Hanelli <hanellidaniele@gmail.com>
2020-10-15 08:48:22 +0200

    Costruzione di un covertitore binario funzionante

---
+++ ConvertitoreBinario/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConvertitoreBinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, resto, divisore;
+            Console.Write("Inserisci il numero decimale:");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Inserisci il valore con cui vuoi dividere:");
+            divisore = Convert.ToInt32(Console.ReadLine());
+            string binario = "";
+            while (num > 0)
+            {
+                resto = num % divisore;
+                num /= divisore;
+                binario = Convert.ToString(resto) + binario;
+            }
+            Console.WriteLine($"Il numero binario corrisponde a: {binario}");
+            Console.ReadLine();
+        }
+    }
+}
